5386c8a55fa4bdefcfcfbe9de202461d6f9e0ad6
Balazs Varga <warnyul@gmail.com>
2018-02-18 10:50:57 +0100

    Fix Retrofit threading problem

--- app/src/main/java/digital/wup/superhero/data/CharactersRemoteDataStore.java
+++ app/src/main/java/digital/wup/superhero/data/CharactersRemoteDataStore.java
@@ -1,6 +1,7 @@
 package digital.wup.superhero.data;
 
 
+import java.io.IOException;
 import java.util.concurrent.Executors;
 
 import digital.wup.superhero.data.model.Character;
@@ -13,6 +14,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.HttpException;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -34,7 +36,6 @@ public class CharactersRemoteDataStore implements CharactersDataStore {
                 .baseUrl("https://gateway.marvel.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(okHttpClient)
-                .callbackExecutor(Executors.newSingleThreadExecutor())
                 .build();
 
         service = retrofit.create(CharacterService.class);
@@ -42,39 +43,34 @@ public class CharactersRemoteDataStore implements CharactersDataStore {
 
     @Override
     public void loadCharacters(Page page, final LoadCharactersCallback callback) {
-        service.loadCharacters(page.getLimit(), page.getOffset()).enqueue(new Callback<CharacterDataWrapper>() {
-            @Override
-            public void onResponse(Call<CharacterDataWrapper> call, Response<CharacterDataWrapper> response) {
-                if (response.body() != null)
-                    callback.onSuccess(response.body().getData().getResults());
-                else
-                    callback.onError(new Error(Error.EMPTY, "response body empty"));
-            }
+        try {
+            Response<CharacterDataWrapper> response = service.loadCharacters(page.getLimit(), page.getOffset()).execute();
 
-            @Override
-            public void onFailure(Call<CharacterDataWrapper> call, Throwable t) {
-                callback.onError(new Error(Error.EMPTY, "failed to load characters"));
-                Timber.e(t);
+            if (response.body() != null) {
+                callback.onSuccess(response.body().getData().getResults());
+            } else {
+                callback.onError(new Error(Error.EMPTY, "response body empty"));
             }
-        });
+        } catch (HttpException | IOException e) {
+            Timber.e(e);
+            callback.onError(new Error(Error.EMPTY, "failed to load characters"));
+        }
     }
 
     @Override
     public void loadCharacter(String id, final LoadCharactersCallback callback) {
-        service.loadCharacter(id).enqueue(new Callback<CharacterDataWrapper>() {
-            @Override
-            public void onResponse(Call<CharacterDataWrapper> call, Response<CharacterDataWrapper> response) {
-                if (response.body() != null)
-                    callback.onSuccess(response.body().getData().getResults());
-                else
-                    callback.onError(new Error(Error.EMPTY, "response body empty"));
+        try {
+             Response<CharacterDataWrapper> response = service.loadCharacter(id).execute();
+            if (response.body() != null) {
+                callback.onSuccess(response.body().getData().getResults());
             }
-
-            @Override
-            public void onFailure(Call<CharacterDataWrapper> call, Throwable t) {
-                callback.onError(new Error());
+            else {
+                callback.onError(new Error(Error.EMPTY, "response body empty"));
             }
-        });
+        } catch (HttpException | IOException e) {
+            Timber.e(e);
+            callback.onError(new Error());
+        }
     }
 
     @Override
